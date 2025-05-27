03c5cd309c53bf041a0583277b3f7547f6a9b8b9
Antônio Rocha <antoniojunior159@gmail.com>
2025-05-27 13:37:52 -0300

    Update Lambda

--- AWSLambda/obj/Debug/net8.0/AWSLambda.AssemblyInfo.cs
+++ AWSLambda/obj/Debug/net8.0/AWSLambda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AWSLambda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e42fb6312dbf17f7b503c400830dea645a93c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0185f595d52c96b37180c17053b892fde4e874d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AWSLambda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AWSLambda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
